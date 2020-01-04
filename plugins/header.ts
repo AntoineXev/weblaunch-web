a1324c6c99283f42dffc8dd776f91969864f5a00
Antoine <antoine.hervet@interact.io>
2020-01-04 20:48:11 +0100

    Added mobile responsivness

--- plugins/header.ts
+++ plugins/header.ts
@@ -8,11 +8,13 @@ export function setHeaderScroll() {
             if (headerClasses && !headerClasses.contains('scrolled')) {
                 headerClasses.add('scrolled');
                 document.getElementById('header-button').classList.remove('white')
+                document.getElementById('mobile-button').classList.remove('white')
             }
         } else {
             if (headerClasses && headerClasses.contains('scrolled')) {
                 headerClasses.remove('scrolled')
                 document.getElementById('header-button').classList.add('white')
+                document.getElementById('mobile-button').classList.add('white')
             }
         }
     }
