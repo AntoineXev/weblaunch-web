d87f445b637d2e4c2fb207c12d6c133257cb9981
Antoine <antoine.hervet@interact.io>
2020-01-07 15:24:50 +0100

    added PopUp and Form

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import {setHeaderScroll} from './plugins/header';
+import {popupClass} from './plugins/popup';
+import {meetingForm} from "./plugins/meeting-form";
+
+window.popup = new popupClass();
+window.meetingForm = new meetingForm();
 
 function main() {
     setHeaderScroll()
 }
 
+
 main();
+
